92ec57a5ed921de8a0d13d80e98a6ae4c00fa1b4
pamelita04 <wen.pame04@gmail.com>
2017-03-09 20:48:39 -0400

    initial layout - welcome

---
+++ client/app/animations.js
@@ -0,0 +1,6 @@
+$(function(){
+  $(".welcome").typed({
+    strings: ["Hi! :) <br>This is my final project<br>I will finished it soon... <br>I can do it!"],
+    typeSpeed: 20
+  });
+});
---
+++ client/firebase-config.js
@@ -0,0 +1,8 @@
+ var config = {
+    apiKey: "AIzaSyD3-Sp7S7q1cvYnL6PHHk0Xgvc8EAPPKIc",
+    authDomain: "myproyectpam.firebaseapp.com",
+    databaseURL: "https://myproyectpam.firebaseio.com",
+    storageBucket: "myproyectpam.appspot.com",
+    messagingSenderId: "763157513119"
+  };
+  firebase.initializeApp(config);
